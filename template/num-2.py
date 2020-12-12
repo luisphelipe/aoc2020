826731d0ff1169a5051c53133d415b89ef590d31
Luis Phelipe <scphelipe@gmail.com>
2020-12-12 07:45:43 -0300

    Modify template to include reading input

--- template/num-2.py
+++ template/num-2.py
@@ -4,6 +4,13 @@ local = os.path.dirname(os.path.abspath(__file__))
 input = os.path.join(local, 'input')
 
 def main ():
-    print("implementation missing")
+    with open(input) as f:
+        lines = f.read().splitlines()
+
+        for line in lines:
+            data = line.split(" ")
+        
+
+        print("implementation missing")
 
 main()
